05dfca5a82d825813113767f8c10fa70812e89e5
Matt Lysek <matt.lysek@gmail.com>
2018-01-07 17:40:52 +0100

    removed old dice rolling

--- bartender.py
+++ bartender.py
@@ -32,11 +32,13 @@ def validate_command(message):
 
 @client.event
 async def on_message(message):
-    roller = dice_roller.DiceRoller()
+    roller = dice_roller.DiceRoller(7)
     if message_starts_with(message, 'sukcesy'):
         await client.send_message(message.channel, '```' + dice_roller.change_success_threshold(message))
     if message_starts_with(message, 'rzuc') or message_starts_with(message, 'rzuć'):
-        await client.send_message(message.channel, '```' + dice_roller.perform_standard_roll(message))
+        print('Stół do rzucania kośćmi gotowy!')
+        result = roller.roll_dice(message)
+        await client.send_message(message.channel, result)
     if message_starts_with(message, 'powiedz'):
         args = message.content.split(" ")
         await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
@@ -44,9 +46,10 @@ async def on_message(message):
         await client.send_message(message.channel, manual.show_help())
     if message_starts_with(message, 'drama') or message_starts_with(message, 'drama!'):
         await client.send_message(message.channel, '```' + dice_roller.perform_drama_roll())
-    if message_starts_with(message, 'r!'):
-        print('Stół do rzucania kośćmi gotowy!')
-        result = roller.roll_dice(message)
+    if message_starts_with(message, 's!'):
+        print('Nowy próg sukcesu')
+        result = roller.change_success_threshold(message)
         await client.send_message(message.channel, result)
+
 
 client.run("Mzg5NzYyMDM5MTc1OTcwODI2.DRARvQ.rYou-m3OjU5c6zvXd4_seyb5GUw")
--- dice_roller.py
+++ dice_roller.py
@@ -3,28 +3,29 @@ import gif_database
 
 
 class DiceRoller:
-    results = [0]
-    success_threshold = 7
 
-    dice_number = 0
+    def __init__(self, given_success_threshold):
+        self.success_threshold = given_success_threshold
+        self.results = [0]
 
-    successes = 0
-    failures = 0
+        self.dice_number = 0
 
-    tens = 0
-    critical_failures = 0
+        self.successes = 0
+        self.failures = 0
 
-    glitch = False
+        self.tens = 0
+        self.critical_failures = 0
 
+        self.glitch = False
+
+    # Dice Rolling
     def roll_dice(self, message):
-        self.__init__()
         self.__prepare_dice(message)
         self.__roll_multiple_times(self.dice_number)
         self.__evaluate_roll_output()
         self.__check_glitch()
         return self.__output_roll_result()
 
-#Dice Rolling
     def __prepare_dice(self, message):
         args = message.content.split(" ")
         if len(args) > 1 and args[1].isdigit():
@@ -67,13 +68,7 @@ class DiceRoller:
         return result
 
 
-
-
-
-
-
 success_threshold = 7
-number_of_potential_rerolls = 0
 
 
 def set_success_threshold_to(new_success_threshold):
@@ -92,75 +87,3 @@ def change_success_threshold(message):
         return 'Porżnęło Cię? Nic nie zmieniam Ty chory pojebie ```'
     set_success_threshold_to(new_threshold)
     return 'W następnym rzucie będę miał sukcesy od ' + str(success_threshold) + '```'
-
-
-def roll_dice():
-    return random.randrange(1, 11)
-
-
-def roll_given_number_of_times(number_of_times):
-    args = [0] * number_of_times
-    for i in range(number_of_times):
-        args[i] = roll_dice()
-    return args
-
-
-def count_successes(roll_output):
-    global number_of_potential_rerolls
-    number_of_successes = 0
-    for i in range(len(roll_output)):
-        if roll_output[i] >= success_threshold:
-            number_of_successes += 1
-            if roll_output[i] == 10:
-                number_of_potential_rerolls += 1
-    return number_of_successes
-
-
-def count_failures(roll_output):
-    number_of_failures = 0
-    for i in range(len(roll_output)):
-        if roll_output[i] == 1:
-            number_of_failures += 1
-    return number_of_failures
-
-
-def check_if_glitch_occurred(number_of_failures, number_of_dice_rolled):
-    return number_of_failures >= (number_of_dice_rolled/2)
-
-
-def perform_roll(number_of_dice):
-    if number_of_dice <= 0:
-        return 'To ile kostek mam rzucić? ```'
-
-    rolled = roll_given_number_of_times(number_of_dice)
-    successes = count_successes(rolled)
-    failures = count_failures(rolled)
-    glitch = check_if_glitch_occurred(failures, len(rolled))
-
-    message_array = 'Rzucam ' + str(len(rolled)) + ' kostkami z sukcesami od ' + str(
-        success_threshold) + '\noto wyniki rzutu: \n' + str(rolled[0:]) + '\n liczba sukcesów: ' + str(successes)
-    if glitch:
-        glitch_message = '\nliczba jedynek: ' + str(failures) + '\nZgrzyt!'
-        message_array += glitch_message
-    if successes == 0:
-        message_array += '``` ' + gif_database.random_fail() + ' ``` Coś nie wyszło...'
-    if successes == len(rolled):
-        message_array += '``` ' + gif_database.random_win() + ' ``` Wow!'
-    return_message = message_array + '```'
-    reset_success_threshold()
-    return return_message
-
-
-def perform_standard_roll(message):
-    args = message.content.split(" ")
-    if len(args) > 1 and args[1].isdigit():
-        number_of_dice = int(args[1])
-        return perform_roll(number_of_dice)
-    return 'Czekaj, ile tych kostek mam rzucić?```'
-
-
-def perform_drama_roll():
-    global number_of_potential_rerolls
-    rerolls = number_of_potential_rerolls
-    number_of_potential_rerolls = 0
-    return "Drama! Przerzucam dziesiątki! \n" + perform_roll(rerolls)

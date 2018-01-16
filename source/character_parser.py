b441de374fae282c51a67ac8fcdd391150d457f7
Matt Lysek <matt.lysek@gmail.com>
2018-01-16 19:25:05 +0100

    import character start

--- source/character_parser.py
+++ source/character_parser.py
@@ -4,3 +4,13 @@ upload and parse characters made in google sheets to character pool
 stuff to use:
 https://github.com/burnash/gspread
 '''
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds']
+creds = ServiceAccountCredentials.from_json_keyfile_name('google_api_secret.json, scope')
+client = gspread.authorize(creds)
+
+sheet = client.open('Character Sheet Concept')
+
